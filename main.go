070be72582b19cd25c63b75394d97deda8fe5988
frederik.steffen <frederik.steffen@gmail.com>
2020-06-09 13:44:26 +0200

    added swagger-ui

--- main.go
+++ main.go
@@ -9,6 +9,11 @@ import (
 
 func main() {
 	router := mux.NewRouter()
+
+	router.Methods("GET").Path("/").Handler(http.RedirectHandler("/swagger-ui/", http.StatusPermanentRedirect))
+	router.PathPrefix("/api-docs/").Handler(http.StripPrefix("/api-docs/", http.FileServer(http.Dir("./api/"))))
+	router.PathPrefix("/swagger-ui/").Handler(http.StripPrefix("/swagger-ui/", http.FileServer(http.Dir("./web/swagger-ui/"))))
+
 	router.Methods("POST").Path("/orders").HandlerFunc(CreateOrder)
 	router.Methods("PUT").Path("/orders/{id}").HandlerFunc(UpdateOrder)
 
